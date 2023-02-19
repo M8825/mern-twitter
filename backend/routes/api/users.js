5937ceda9e9e3ee734c95dd5eff715c48e9e3ebf
Mlkz <maho.mamulashvili@gmail.com>
2023-02-18 23:35:50 -0500

    Add seed file for users and tweets

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -69,6 +69,7 @@ router.post('/register', validateRegisterInput, async (req, res, next) => {
 
 // POST /api/users/login
 router.post('/login', validateLoginInput, async (req, res, next) => {
+
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
